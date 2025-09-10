9be52f714e0bf4c639330d3704c525217e9c63f3
shahinali-dev <shahinali.dev@gmail.com>
2025-09-10 14:42:35 +0600

    feat: Update Project Details

--- src/components/project-detilas/projectDetailsHeader.ts
+++ src/components/project-detilas/projectDetailsHeader.ts
@@ -6,22 +6,20 @@ const ProjectDetailsHeader = (
   logo,
   description,
   previewUrl
-) => html` <div
-  class="relative h-[400px] lg:h-[500px] overflow-hidden rounded-b-3xl"
->
+) => html` <div class="relative h-[400px] lg:h-[500px] overflow-hidden">
   <img
     src="${image}"
     alt="${title}"
     class="absolute inset-0 w-full h-full object-cover"
   />
-  <div class="absolute inset-0 bg-black/50"></div>
+  <div class="absolute inset-0 bg-black/80"></div>
   <div
     class="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4"
   >
     <img
       src="${logo}"
       alt="${title} logo"
-      class="h-16 w-16 object-contain mb-6 bg-white rounded-xl p-2 shadow-lg"
+      class="h-16 w-16 object-contain mb-6 bg-white rounded-xl  shadow-lg"
     />
     <h1 class="text-4xl lg:text-6xl font-bold mb-4">${title}</h1>
     <p class="text-lg max-w-2xl">${description}</p>
