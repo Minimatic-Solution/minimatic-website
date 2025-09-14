bd06a1ab744b956330db6c838f42a1481e436f91
shahinali-dev <shahinali.dev@gmail.com>
2025-09-14 22:18:48 +0600

    feat: Implement favicon directory as serveStatic

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import app from './index'
-import { serve } from '@hono/node-server'
-import { serveStatic } from '@hono/node-server/serve-static'
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+import app from "./index";
 
-const port = Number(process.env.PORT) || 3000
+const port = Number(process.env.PORT) || 3000;
 
 // Serve static files from public directory
 app.use("/css/*", serveStatic({ root: "./public" }));
 app.use("/js/*", serveStatic({ root: "./public" }));
 app.use("/images/*", serveStatic({ root: "./public" }));
+app.use("/favicon/*", serveStatic({ root: "./public" }));
 
+serve({ fetch: app.fetch, port });
 
-serve({ fetch: app.fetch, port })
-
-console.log(`Server running on http://0.0.0.0:${port}`)
+console.log(`Server running on http://0.0.0.0:${port}`);
